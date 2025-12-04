6288cbd51a4e4ec9a709bf79160ed42f414cd2dd
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2025-12-04 15:05:41 +0100

    inserimento di while per far funzionare il ciclo

--- script-dadi.js
+++ script-dadi.js
@@ -1,3 +1,6 @@
+let continua = true; // input per permettere all'utente di terminare la partita
+
+while (continua) {
 // devo generare dei numeri random per l'utente 1-6
 let numeroUtente = Math.floor(Math.random() * 6) + 1;
 console.log("Il tuo numero è ", numeroUtente);
@@ -7,10 +10,17 @@ let numeroComputer = Math.floor(Math.random() * 6) + 1;
 console.log("Il numero dell'avversario è ", numeroComputer);
 
 // devo stabilire chi ha vinto confrontando i 2 numeri e prendendo quello più grande
-if (numeroUtente > numeroComputer) {
-    console.log("Hai vinto!!!");
-} else if (numeroUtente === numeroComputer) {
-    console.log("Pareggio... riprova!")
-} else {
-    console.log("Hai perso")
+    if (numeroUtente > numeroComputer) {
+        console.log("Hai vinto!!!");
+    } else if (numeroUtente === numeroComputer) {
+        console.log("Pareggio... riprova!");
+    } else {
+        console.log("Hai perso");
+    }
+
+    let continuaPartita = prompt("Vuoi fare un'altra partita?");
+    if (continuaPartita !== "si") {
+        console.log("Gioco terminato!");
+        continua = false;
+    }
 }
